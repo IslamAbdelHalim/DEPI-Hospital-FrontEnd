56c6522cc43828b5c22fa807b426f18afcd286ac
Hamada Ashour <hamada.ashour@dxc.com>
2024-09-19 16:31:54 +0300

    deprtmants after inplamnet

--- src/app/componants/departmentes/departmentes.component.ts
+++ src/app/componants/departmentes/departmentes.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsDataService } from '../../services/departments-data.service';
 
 @Component({
   selector: 'app-departmentes',
   templateUrl: './departmentes.component.html',
-  styleUrl: './departmentes.component.css'
+  styleUrl: './departmentes.component.css',
 })
-export class DepartmentesComponent {
+export class DepartmentesComponent implements OnInit{
+  departments: any[] = [];
+
+  constructor(private ds: DepartmentsDataService) {}
+
+  ngOnInit(): void {
+    this.departments = this.ds.getDepartments();
+  }
+
 
 }
