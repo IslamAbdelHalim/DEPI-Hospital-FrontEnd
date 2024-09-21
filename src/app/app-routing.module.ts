0e321c50a41e64052865222fa0f78d419d96086f
Hamada Ashour <hamada.ashour@dxc.com>
2024-09-21 15:54:20 +0300

    eachdepertment finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentesComponent } from './componants/departmentes/departmentes.component';
+import { EachDepartmentComponent } from './componants/each-department/each-department.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DepartmentesComponent },
+  { path: 'departmentes', component:DepartmentesComponent },
+  { path: 'department/:departmentLinke', component: EachDepartmentComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/componants/each-department/each-department.component.ts
+++ src/app/componants/each-department/each-department.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { DepartmentsDataService } from '../../services/departments-data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-each-department',
   templateUrl: './each-department.component.html',
-  styleUrl: './each-department.component.css'
+  styleUrl: './each-department.component.css',
 })
 export class EachDepartmentComponent {
+  departments: any[] = [];
+  selectedDepartment: any;
+  remainingDepartments: any[] = [];
 
+  constructor(
+    private ds: DepartmentsDataService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getDepartments();
+    this.selectDepartment();
+    this.getRemainingDepartments();
+  }
+
+  getDepartments() {
+    this.departments = this.ds.getDepartments();
+  }
+
+  selectDepartment() {
+    this.route.paramMap.subscribe((params) => {
+      const departmentLink = params.get('departmentLinke');
+      if (departmentLink) {
+        this.selectedDepartment = this.departments.find(
+          (dept) =>
+            dept.link.toLowerCase() ===
+            ('/department/' + departmentLink).toLowerCase()
+        );
+      }
+    });
+  }
+
+  getRemainingDepartments() {
+    const allDepartments = this.ds.getDepartments();
+    const filteredDepartments = allDepartments.filter(
+      (dept) => dept.id !== this.selectedDepartment?.id
+    );
+    const shuffledDepartments = filteredDepartments.sort(() => 0.5 - Math.random());
+    this.remainingDepartments = shuffledDepartments.slice(0, 4);
+  }
+  
 }
