2b57573d83ba54762e9b63977613c144ee66fa93
Hamada Ashour <hamada.ashour@dxc.com>
2024-09-23 06:07:43 +0300

    initial userprofile branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentesComponent } from './componants/departmentes/departmentes.component';
 import { EachDepartmentComponent } from './componants/each-department/each-department.component';
+import { UserProfileComponent } from './componants/user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '', component: DepartmentesComponent },
   { path: 'departmentes', component:DepartmentesComponent },
-  { path: 'department/:departmentLinke', component: EachDepartmentComponent } 
+  { path: 'department/:departmentLinke', component: EachDepartmentComponent } ,
+  { path: 'userprofile', component: UserProfileComponent } ,
 ];
 
 @NgModule({
