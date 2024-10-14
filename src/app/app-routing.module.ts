e8854acdbc499c4cdf4c145ffa254b4d49b84945
Hamada Ashour <hamada.ashour@dxc.com>
2024-10-14 11:08:20 +0300

    edit the paths of doctor pages and login and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { EachDepartmentComponent } from './componants/each-department/each-depar
 import { UserProfileComponent } from './componants/user-profile/user-profile.component';
 import {HomeComponent} from "./componants/home/home.component";
 import {LoginComponent} from "./componants/login/login.component";
+import { DoctorsComponent } from './componants/doctors/doctors.component';
+import { SignupComponent } from './componants/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'departments', component:DepartmentesComponent },
   { path: 'department/:departmentLinke', component: EachDepartmentComponent } ,
   { path: 'userprofile', component: UserProfileComponent },
+  { path: 'doctors', component:DoctorsComponent },
 ];
 
 @NgModule({
